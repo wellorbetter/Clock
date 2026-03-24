e8183fddb45c29f0c4da46482bcbda9470af3557
wellorbetter <1419919418@qq.com>
2026-03-25 03:55:36 +0800

    fix #289: add localized date format
    remove excessive editing

--- app/src/main/kotlin/org/fossify/clock/fragments/ClockFragment.kt
+++ app/src/main/kotlin/org/fossify/clock/fragments/ClockFragment.kt
@@ -109,7 +109,6 @@ class ClockFragment : Fragment() {
     private fun updateDate() {
         calendar = Calendar.getInstance()
         val formattedDate = requireContext().getFormattedDate(calendar)
-        binding.clockDate.text = formattedDate
         (binding.timeZonesList.adapter as? TimeZonesAdapter)?.todayDateString = formattedDate
     }
 
